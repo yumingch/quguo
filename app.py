3d35c7aec72cfba862f94670aaa2269dc03a7953
Yuming Cheng <ymxcheng@gmail.com>
2015-10-30 12:11:17 +0800

    国家搜索联想完成

---
+++ app.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import leancloud
+leancloud.init('TYH9McMkdViVPOs22r9ndqgP', master_key='dzHshmOz5TQ6QM04pQMm3A6a')
+
+from flask import Flask, render_template, Blueprint
+from leancloud import Object, Query, LeanCloudError
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
