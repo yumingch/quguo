339b3d23fab7068fe1161046bee9007f0cf00745
Yuming Cheng <ymxcheng@gmail.com>
2015-11-06 11:38:29 +0800

    中国地图添加

--- app.py
+++ app.py
@@ -2,18 +2,44 @@
 import leancloud
 leancloud.init('TYH9McMkdViVPOs22r9ndqgP', master_key='dzHshmOz5TQ6QM04pQMm3A6a')
 
-from flask import Flask, render_template, Blueprint
-from leancloud import Object, Query, LeanCloudError, User
+from flask import *
+from leancloud import *
 
 app = Flask(__name__)
 
-user=User()
-
-
+class SavedUserMap(Object):
+	@property
+	def visited_countries(self):
+	    return self.get('visited_countries')
+	@visited_countries.setter
+	def visited_countries(self, value):
+		return self.set('visited_countries', value)
+	@property
+	def email(self):
+	    return self.get('email')
+	@email.setter
+	def email(self, value):
+		return self.set('email', value)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/<user_id>/')
+def user(user_id):
+	query = Query(SavedUserMap)
+	usermap = query.get(user_id)
+	return ('first country is %s' % usermap.visited_countries[0])
+@app.route('/add/', methods=['POST'])
+def add():
+	if request.method == 'POST':
+		title = request.json['title']
+		usermap = SavedUserMap(visited_countries=title)
+		usermap.save()
+	return jsonify(title=title)
+
+
+
 
 #if __name__ == '__main__':
 #    app.debug = True
--- static/quguo.js
+++ static/quguo.js
@@ -1,59 +1,94 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    window.visited = []
 
+    $('#vmap').vectorMap({
+        map: 'world_en',
+        backgroundColor: 'black',
+        borderColor: '#818181',
+        borderOpacity: 0.25,
+        borderWidth: 1,
+        color: 'grey',
+        enableZoom: false,
+        selectedRegions: [],
+        hoverColor: 'grey',
+        hoverOpacity: '0.85',
+        normalizeFunction: 'linear',
+        scaleColors: ['grey', 'grey'],
+        selectedColor: '#99CCFF',
+        showTooltip: true,
+        multiSelectRegion: true,
+    });
 
-
-    $('#vmap').vectorMap({ map: 'world_en',
-    backgroundColor: 'black',
-    borderColor: '#818181',
-    borderOpacity: 0.25,
-    borderWidth: 1,
-    color: 'grey',
-    enableZoom: true,
-    selectedRegions: [],
-    hoverColor: 'grey',
-    hoverOpacity: '0.85',
-    normalizeFunction: 'linear',
-    scaleColors: ['grey', 'grey'],
-    selectedColor: '#99CCFF',
-    showTooltip: true,
-    multiSelectRegion: true,
+    $('#vmapcn').vectorMap({
+        map: 'china_en',
+        backgroundColor: 'black',
+        borderColor: '#818181',
+        borderOpacity: 0.25,
+        borderWidth: 1,
+        color: 'grey',
+        enableZoom: false,
+        selectedRegions: [],
+        hoverColor: 'grey',
+        hoverOpacity: '0.85',
+        normalizeFunction: 'linear',
+        scaleColors: ['grey', 'grey'],
+        selectedColor: '#99CCFF',
+        showTooltip: true,
+        multiSelectRegion: true,
     });
 
     var bsSuggest = $("#countrySearch").bsSuggest({
-        url: "http://ac-tyh9mcmk.clouddn.com/f097bc9fc882e7c0.json",
-        effectiveFields: ["c+e"],
-        searchFields: ["eng"],
+        url: "http://ac-tyh9mcmk.clouddn.com/ef7cf1fff7744e6b.json",
+        effectiveFields: ["continent", "chs"],
+        effectiveFieldsAlias: {
+            "continent": "  ",
+            "chs": "找到"
+        },
+        searchFields: ["eng", "c+e"],
         showBtn: false,
         idField: "id",
         keyField: "chs",
         autoMinWidth: false,
         inputWarnColor: 'white',
         listStyle: {
-                    "padding-top": 0, "max-height": "150px", "max-width": "300px",
-                    "overflow": "auto", "width": "auto",
-                    "transition": "0.3s", "-webkit-transition": "0.3s", "-moz-transition": "0.3s", "-o-transition": "0.3s"
-                },    
-    }).on('onSetSelectValue', function (e, keyword) {
-       id1 = keyword.id.toLowerCase();
-       window.visited = $('#vmap').vectorMap('getS');
-       if(jQuery.inArray(id1, window.visited)==-1){
-            $('#remove').append("<button class = \"btn btn-sm btn-primary zz\" id="+id1+ ">" + keyword.key + "<span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></button>")
+            "padding-top": 0,
+            "max-height": "150px",
+            "max-width": "300px",
+            "overflow": "auto",
+            "width": "100px",
+            "transition": "0.3s",
+            "-webkit-transition": "0.3s",
+            "-moz-transition": "0.3s",
+            "-o-transition": "0.3s"
+        },
+    }).on('onSetSelectValue',
+    function(e, keyword) {
+        id1 = keyword.id.toLowerCase();
+        window.visited = $('#vmap').vectorMap('getS').concat($('#vmapcn').vectorMap('getS'));
+        if (jQuery.inArray(id1, window.visited) == -1) {
+            $('#remove').append("<button class = \"btn btn-sm btn-primary zz\" id=" + id1 + ">" + keyword.key + "<span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></button>")
         };
-       $('#vmap').vectorMap('select', keyword.id.toLowerCase());
-  
-       // $('#remove').text('删除已选择国家(' + window.visited.length + ')');
-       $('input').val("");
-       $('.zz').click(function(){
-            $('#vmap').vectorMap('deselect',this.id);
-            $(this).remove();     
-        
+        if (id1.length == 2) {
+            $('#vmap').vectorMap('select', keyword.id.toLowerCase());
+        } else {
+            $('#vmapcn').vectorMap('select', keyword.id.toLowerCase());
+        }
+
+        // $('#remove').text('删除已选择国家(' + window.visited.length + ')');
+        $('input').val("");
+        $('.zz').click(function() {
+            if (this.id.length == 2){
+                $('#vmap').vectorMap('deselect', this.id);
+                $(this).remove();} else {
+                $('#vmapcn').vectorMap('deselect', this.id);
+                $(this).remove();    
+                }
+
+        });
+        window.visited = $('#vmap').vectorMap('getS').concat($('#vmapcn').vectorMap('getS'));
     });
 
-
-
-    });       
-
-
+});
 /*    $('#remove').click(function(){
         if(window.visited){
             $('#selected').text("");
@@ -63,5 +98,19 @@ $(document).ready(function(){
         };
     }); 
 */
+
+$('#add_new').click(function() {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: "/add/",
+        data: JSON.stringify({
+            title: window.visited
+        }),
+        success: function(data) {
+            console.log(data.title);
+        },
+        dataType: "json"
+    });
 
 });
